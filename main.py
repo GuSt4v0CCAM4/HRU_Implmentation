560b38f175e95fd42c0318fe8856188018fd48fe
GuSt4v0CCAM4 <alonsospinola70@gmail.com>
2024-10-17 07:48:22 -0500

    base HRU

---
+++ main.py
@@ -0,0 +1,28 @@
+class Usuario:
+    def __init__(self, nombre, nivel_de_seguridad):
+        self.nombre = nombre
+        self.nivel_de_seguridad = nivel_de_seguridad
+class Recurso:
+    def __init__(self, nombre, nivel):
+        self.nombre = nombre
+        self.nivel = nivel
+class HRU:
+    def __init__(self):
+        self.usuarios = []
+        self.recursos = []
+    def agregar_usuario(self, usuario):
+        self.usuarios.append(usuario)
+    def agregar_recurso(self, recurso):
+        self.recursos.append(recurso)
+    def acceso_recurso(self, usuario, recurso):
+        if usuario.nivel_de_seguridad >= recurso.nivel:
+            return f"{usuario.nombre} tiene acceso a {recurso.nombre}"
+        else:
+            return f"{usuario.nombre} denegado acceso a {recurso.nombre}"
+    def eliminar_usuario(self, usuario): #metodo para eliminar usuario
+        if usuario in self.usuarios: #primero verificamos si existe el usuario
+            self.usuarios.remove(usuario) #lo eliminmos
+hru = HRU()
+hru.agregar_usuario(Usuario("UsuarioHRU1", 2))
+hru.agregar_usuario(Usuario("UsuarioHRU2", 1))
+hru.agregar_recurso(Recurso("TopSecret", 2))
